2ab24245848b4737937fe0bb271061198c5b1f4a
Dmitriy Ivanov <dmitriyivanov2000@yandex.ru>
2020-07-28 14:50:13 +0400

    Add report for game

---
+++ src/report/Report.java
@@ -0,0 +1,18 @@
+package report;
+
+public class Report {
+
+	private final String msg;
+
+	public Report(String msg) {
+		this.msg = msg;
+	}
+
+	public String getReport() {
+		return msg;
+	}
+
+	public String toString() {
+		return msg;
+	}
+}
---
+++ src/report/ReportTurn.java
@@ -0,0 +1,28 @@
+package report;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ReportTurn {
+
+	private BlockingQueue<Report> queue = new LinkedBlockingQueue<Report>();
+
+	Report accept() {
+
+		while (true) {
+			
+			try {
+				return (queue.take());
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+
+		}
+	}
+	
+	public void offerQueue(Report report) {
+		
+		queue.offer(report);
+	}
+	
+}
